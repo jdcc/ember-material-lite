bd76c1537c3d6b9f68e3d94bb2c6f81ae4d3cbc1
jdcc <jclark@cyber.law.harvard.edu>
2017-09-19 18:08:32 -0400

    removing mouse leave cuz i dunno

--- addon/extensions/tooltip.js
+++ addon/extensions/tooltip.js
@@ -13,12 +13,10 @@ window.MaterialTooltip.prototype.init = function() {
 
     if (this.forElement_) {
       this.boundMouseEnterHandler = this.handleMouseEnter_.bind(this);
-      this.boundMouseLeaveHandler = this.handleMouseLeave_.bind(this);
       this.forElement_.addEventListener('mouseenter', this.boundMouseEnterHandler,
           false);
       this.forElement_.addEventListener('click', this.boundMouseEnterHandler,
           false);
-      this.forElement_.addEventListener('mouseleave', this.boundMouseLeaveHandler);
     }
   }
 };
