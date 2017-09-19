ef21d661141383fb56429a9b600067499e030eae
jdcc <jclark@cyber.law.harvard.edu>
2017-09-19 18:19:12 -0400

    Revert "removing mouse leave cuz i dunno"
    This reverts commit bd76c1537c3d6b9f68e3d94bb2c6f81ae4d3cbc1.

--- addon/extensions/tooltip.js
+++ addon/extensions/tooltip.js
@@ -13,10 +13,12 @@ window.MaterialTooltip.prototype.init = function() {
 
     if (this.forElement_) {
       this.boundMouseEnterHandler = this.handleMouseEnter_.bind(this);
+      this.boundMouseLeaveHandler = this.handleMouseLeave_.bind(this);
       this.forElement_.addEventListener('mouseenter', this.boundMouseEnterHandler,
           false);
       this.forElement_.addEventListener('click', this.boundMouseEnterHandler,
           false);
+      this.forElement_.addEventListener('mouseleave', this.boundMouseLeaveHandler);
     }
   }
 };
